4de887c929592f086fe346a642cb08d8f85b4f54
Og'abek <91404313+Darkfile-beka@users.noreply.github.com>
2022-04-23 12:57:03 +0500

    BekaCoder

---
+++ javascript/main.js
@@ -0,0 +1,160 @@
+
+
+// ——————————————————————————————————————————————————
+// TextScramble
+// ——————————————————————————————————————————————————
+
+    class TextScramble {
+        constructor(el) {
+        this.el = el
+        this.chars = '!<>-_\\/[]{}—=+*^?#________'
+        this.update = this.update.bind(this)
+        }
+        setText(newText) {
+        const oldText = this.el.innerText
+        const length = Math.max(oldText.length, newText.length)
+        const promise = new Promise((resolve) => this.resolve = resolve)
+        this.queue = []
+        for (let i = 0; i < length; i++) {
+            const from = oldText[i] || ''
+            const to = newText[i] || ''
+            const start = Math.floor(Math.random() * 40)
+            const end = start + Math.floor(Math.random() * 40)
+            this.queue.push({ from, to, start, end })
+        }
+        cancelAnimationFrame(this.frameRequest)
+        this.frame = 0
+        this.update()
+        return promise
+        }
+        update() {
+        let output = ''
+        let complete = 0
+        for (let i = 0, n = this.queue.length; i < n; i++) {    
+            let { from, to, start, end, char } = this.queue[i]
+            if (this.frame >= end) {
+            complete++
+            output += to
+            } else if (this.frame >= start) {
+            if (!char || Math.random() < 0.28) {
+                char = this.randomChar()
+                this.queue[i].char = char
+            }
+            output += `<span class="dud">${char}</span>`
+            } else {
+            output += from
+            }
+        }
+        this.el.innerHTML = output
+        if (complete === this.queue.length) {
+            this.resolve()
+        } else {
+            this.frameRequest = requestAnimationFrame(this.update)
+            this.frame++
+        }
+        }
+        randomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)]
+        }
+    }
+    
+    //    ABOUT TEXT ANIMATION
+
+
+    // ——————————————————————————————————————————————————
+    // Example
+    // ——————————————————————————————————————————————————
+    
+    const phrases = [
+        'Web Developer',
+        'Master HTML',
+        'Master CSS',
+        'Junior JS ',
+        'CMD',
+        'Kali Linux',
+        'Termux',
+        'Adobe XD',
+        'Figma',
+        'UI / UX'
+    ]
+    
+    const el = document.querySelector('.typing')
+    const fx = new TextScramble(el)
+    
+    let counter = 0
+    const next = () => {
+        fx.setText(phrases[counter]).then(() => {
+        setTimeout(next, 800)
+        })
+        counter = (counter + 1) % phrases.length
+    }
+    
+    next()
+
+
+
+
+
+    // ANIMATIOnN ABOUT TEXT 
+
+
+    
+
+
+//   animate All Text Hacking
+
+consoleText(['Sayt shaxsiy malimot berish maqsida tuzilgan va tuzuvchi Khudoyberdiev Ogabek. Siz sayt orqali men xaqimda malimot va boglabishingiz mumkin'], 'text',['tomato','rebeccapurple','lightblue']);
+
+function consoleText(words, id, colors) {
+  if (colors === undefined) colors = ['#fff'];
+  var visible = true;
+  var con = document.getElementById('console');
+  var letterCount = 1;
+  var x = 1;
+  var waiting = false;
+  var target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+  window.setInterval(function() {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(function() {
+        var usedColor = colors.shift();
+        colors.push(usedColor);
+        var usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function() {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+    }
+  }, 120)
+  window.setInterval(function() {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+
+      visible = true;
+    }
+  }, 400)
+}
+
+
+
+
+
